6f391bbeae909510fcdf09429b67ef3f3c188f71
Rehab <rehabtariqq@gmail.com>
2026-09-19 13:53:15 +0500

    feat: add user input handling

--- chatbot.cpp
+++ chatbot.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
 
     cout << "Hello! I am Reva, your rule-based AI chatbot." << endl;
+
+    string userInput;
+
+    cout << "You: ";
+    getline(cin, userInput);
+
+    cout << "You said: " << userInput << endl;
 
     return 0;
 }
