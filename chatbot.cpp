daa5f4a2f5d0fb7a73649b7d29e0b6cc8d3293dd
Rehab <rehabtariqq@gmail.com>
2026-09-19 13:56:17 +0500

    feat: add chatbot response rules

--- chatbot.cpp
+++ chatbot.cpp
@@ -11,8 +11,24 @@ int main() {
     cout << "You: ";
     getline(cin, userInput);
 
-    if (userInput == "hello") {
+    if (userInput == "hello" || userInput == "hi" || userInput == "hey") {
         cout << "Reva: Hello! Nice to meet you." << endl;
+    }
+
+    else if (userInput == "how are you") {
+        cout << "Reva: I'm doing great! Thanks for asking." << endl;
+    }
+
+    else if (userInput == "what is your name") {
+        cout << "Reva: My name is Reva." << endl;
+    }
+
+    else if (userInput == "what can you do") {
+        cout << "Reva: I can respond to predefined messages using simple rules." << endl;
+    }
+
+    else {
+        cout << "Reva: Sorry, I don't understand that yet." << endl;
     }
 
     return 0;
