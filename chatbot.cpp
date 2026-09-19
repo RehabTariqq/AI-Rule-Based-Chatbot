d6181f71a8d9f672aefb083759f5bf50df2424b6
Rehab <rehabtariqq@gmail.com>
2026-09-19 13:58:33 +0500

    feat: add continuous conversation loop

--- chatbot.cpp
+++ chatbot.cpp
@@ -6,29 +6,32 @@ int main() {
 
     cout << "Hello! I am Reva, your rule-based AI chatbot." << endl;
 
-    string userInput;
+    while (true) {
 
-    cout << "You: ";
-    getline(cin, userInput);
+        string userInput;
 
-    if (userInput == "hello" || userInput == "hi" || userInput == "hey") {
-        cout << "Reva: Hello! Nice to meet you." << endl;
-    }
+        cout << "You: ";
+        getline(cin, userInput);
 
-    else if (userInput == "how are you") {
-        cout << "Reva: I'm doing great! Thanks for asking." << endl;
-    }
+        if (userInput == "hello" || userInput == "hi" || userInput == "hey") {
+            cout << "Reva: Hello! Nice to meet you." << endl;
+        }
 
-    else if (userInput == "what is your name") {
-        cout << "Reva: My name is Reva." << endl;
-    }
+        else if (userInput == "how are you") {
+            cout << "Reva: I'm doing great! Thanks for asking." << endl;
+        }
 
-    else if (userInput == "what can you do") {
-        cout << "Reva: I can respond to predefined messages using simple rules." << endl;
-    }
+        else if (userInput == "what is your name") {
+            cout << "Reva: My name is Reva." << endl;
+        }
 
-    else {
-        cout << "Reva: Sorry, I don't understand that yet." << endl;
+        else if (userInput == "what can you do") {
+            cout << "Reva: I can respond to predefined messages using simple rules." << endl;
+        }
+
+        else {
+            cout << "Reva: Sorry, I don't understand that yet." << endl;
+        }
     }
 
     return 0;
